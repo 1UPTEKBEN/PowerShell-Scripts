1c1d499288629db626b9ebed334ae35d0a38e3a8
Ben <44791946+1UPTEKBEN@users.noreply.github.com>
2024-12-04 12:52:52 +1000

    Update KeepAlive.ps1
    Changed it from 10 Seconds to 1 Second.
    
    Fixed Array Issue

--- KeepAlive.ps1
+++ KeepAlive.ps1
@@ -2,14 +2,15 @@ function Format-TimeSpan {
     param (
         [System.TimeSpan]$TimeSpan
     )
-    
     "{0:D2}:{1:D2}:{2:D2}" -f $TimeSpan.Hours, $TimeSpan.Minutes, $TimeSpan.Seconds
 }
 
 $startTime = Get-Date
 
+# Allocate and initialize memory
 $PowerRequest = [System.Runtime.InteropServices.Marshal]::AllocHGlobal(64)
-[System.Runtime.InteropServices.Marshal]::Copy([byte[]](0,0,0,0,0,0,0,0), 0, $PowerRequest, 64)
+$byteArray = New-Object byte[] (64)
+[System.Runtime.InteropServices.Marshal]::Copy($byteArray, 0, $PowerRequest, 64)
 
 Add-Type @"
 using System;
@@ -25,15 +26,18 @@ $ES_SYSTEM_REQUIRED = 0x00000001
 $ES_DISPLAY_REQUIRED = 0x00000002
 $ES_AWAYMODE_REQUIRED = 0x00000040
 
+try {
+    while ($true) {
+        [PowerManagement]::SetThreadExecutionState($ES_CONTINUOUS -bor $ES_SYSTEM_REQUIRED -bor $ES_DISPLAY_REQUIRED -bor $ES_AWAYMODE_REQUIRED) | Out-Null
 
-while ($true) {
-    [PowerManagement]::SetThreadExecutionState($ES_CONTINUOUS -bor $ES_SYSTEM_REQUIRED -bor $ES_DISPLAY_REQUIRED -bor $ES_AWAYMODE_REQUIRED) | Out-Null
+        $elapsedTime = (Get-Date) - $startTime
+        $formattedTime = Format-TimeSpan -TimeSpan $elapsedTime
 
-    $elapsedTime = (Get-Date) - $startTime
-    $formattedTime = Format-TimeSpan -TimeSpan $elapsedTime
+        Write-Host "Script has been running for $formattedTime"
 
-    Write-Host "Script has been running for $formattedTime"
-
-    Start-Sleep -Seconds 10
+        Start-Sleep -Seconds 1
+    }
+} finally {
+    # Free the allocated memory
+    [System.Runtime.InteropServices.Marshal]::FreeHGlobal($PowerRequest)
 }
-[System.Runtime.InteropServices.Marshal]::FreeHGlobal($PowerRequest)
